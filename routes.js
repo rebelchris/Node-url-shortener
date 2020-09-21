82bb14ab931212eec55f5c110c187210b70e16b5
Chris Bongers <chrisbongers@gmail.com>
2020-09-21 11:23:14 +0200

    Added chris route

--- routes.js
+++ routes.js
@@ -9,4 +9,8 @@ router.get("/about", function (req, res) {
     res.send("Very cool tool, multi pages even work");
 });
 
+router.get("/chris", function (req, res) {
+    res.send("Chris is a cool guy");
+});
+
 module.exports = router;
